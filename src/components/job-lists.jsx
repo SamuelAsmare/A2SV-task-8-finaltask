7d85e92a62dc9c347826d0aaf2eca03b19832955
SamuelAsmare <samuelasmare19@gmail.com>
2025-07-28 10:21:35 +0300

    initial commit

--- src/components/job-lists.jsx
+++ src/components/job-lists.jsx
@@ -13,12 +13,12 @@ const JobLists = () => {
         <div className="border-1 border-gray-200 rounded-xl p-6 flex flex-col gap-5 cursor-pointer
          transition duration-300 ease-in-out justify-between w-full lg:w-3/4 hover:shadow-lg "
          onClick={()=>Go(index)}>
-          {/* avater and title */}
+          {/* avater and title div*/}
           <div className="flex gap-10"> 
               <img src={job.image} className="w-10 h-10 rounded-full"/>
           {/* title and location */}
               <div>
-                <h1 className="font-semibold ">{job.title}</h1>
+                <h1 className="font-bold ">{job.title}</h1>
                 <p className= "text-xs">{job.company}.  {job.about.location}</p>
               </div>
           </div>
