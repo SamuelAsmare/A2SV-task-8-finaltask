d8745e0b2d4703158db3ec9d3af30f8e535e4074
SamuelAsmare <samuelasmare19@gmail.com>
2025-08-05 18:43:26 +0300

    Fix: Correct Tailwind setup for Netlify-

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -10,4 +13,3 @@ export default {
   },
   plugins: [],
 }
-
